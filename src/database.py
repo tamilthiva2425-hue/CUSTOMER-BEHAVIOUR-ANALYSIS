c57a0b1f206cfebecdb9d19898f48bd970e82d03
Tamilselvan A <tamilthiva2425@gmail.com>
2026-09-17 17:27:30 +0530

    Secure database credentials with environment variables

--- src/database.py
+++ src/database.py
@@ -1,12 +1,12 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 
+load_dotenv()
+
 connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-password=""
-    database="customer_behaviour"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
-
-print("MySQL connection successful!")
-
-connection.close()
